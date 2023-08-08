7789f14ab675de22bbb2e339c0714e4f6d067b7d
Marc <marcbaque1311@gmail.com>
2023-08-09 00:10:05 +0200

    [ ADD ] initial list of upcoming dividends of a portfolio

--- src/calculation.ts
+++ src/calculation.ts
@@ -24,3 +24,7 @@ export function getValueOfPortfolio(
     totalValue: formatCurrency(totalValue)
   }
 }
+
+export function getPercentage(value1: number, value2: number) {
+  return `${((value1 / value2) * 100).toFixed(2)}%`;
+}
