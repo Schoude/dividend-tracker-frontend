ec5a0504f39a4b79fe93f871b214f40d38dd8eb8
Marc <marcbaque1311@gmail.com>
2023-08-24 22:32:47 +0200

    [ ADD ] notation param to number format

--- src/utils/intl.ts
+++ src/utils/intl.ts
@@ -1,11 +1,14 @@
+
 export function formatCurrency(
   value: number,
   currency: "USD" | "EUR" = "USD",
   locale: "en-US" | "de-DE" = "en-US",
+  notation?: "standard" | "scientific" | "engineering" | "compact" | undefined,
 ): string {
   return new Intl.NumberFormat(locale, {
     style: "currency",
     currency,
+    notation,
   })
     .format(value);
 }
