b0212ac66a34cf66271b31e2d217e811f0e016a2
Marc <marcbaque1311@gmail.com>
2023-08-29 22:47:21 +0200

    [ UPDATE ] fetch data using Promise.allSettled()

---
+++ src/utils/type-guards.ts
@@ -0,0 +1,7 @@
+export function isFulfilled<T>(promise: PromiseSettledResult<T>): promise is PromiseFulfilledResult<T> {
+  return promise.status === 'fulfilled'
+};
+
+export function isRejected<T>(promise: PromiseSettledResult<T>): promise is PromiseRejectedResult {
+  return promise.status === 'rejected'
+};
