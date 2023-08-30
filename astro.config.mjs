619d7f852b0bde8412b4db7a3ccfaa75c99e33e3
Marc <marcbaque1311@gmail.com>
2023-08-30 21:20:56 +0200

    [ UPDATE ] astro & add vue transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
   output: "server",
   adapter: deno(),
   integrations: [vue()],
