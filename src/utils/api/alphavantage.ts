23fe20aaf4157ecb3f429b4f7d6659a669fae368
Marc <marcbaque1311@gmail.com>
2023-09-06 22:31:33 +0200

    [ ADD ] validations & types for sentiment news

--- src/utils/api/alphavantage.ts
+++ src/utils/api/alphavantage.ts
@@ -1,3 +1,32 @@
+import { object, type Output, parse, string, number, array, enumType, ValiError, union, nullType } from 'valibot';
+
+const ArticleNewsSentimentSchema = object({
+  title: string(),
+  url: string(),
+  time_published: string(),
+  authors: array(string()),
+  summary: string(),
+  banner_image: union([string(), nullType()]),
+  source: string(),
+  source_domain: string(),
+  overall_sentiment_score: number(),
+  overall_sentiment_label: enumType(["Bearish", "Bullish", "Neutral", "Somewhat-Bearish", "Somewhat-Bullish"]),
+  ticker_sentiment: array(object({
+    ticker: string(),
+    relevance_score: string(),
+    ticker_sentiment_score: string(),
+    ticker_sentiment_label: enumType(["Bearish", "Bullish", "Neutral", "Somewhat-Bearish", "Somewhat-Bullish"]),
+  })),
+  topics: array(object({
+    topic: string(),
+    relevance_score: string(),
+  }))
+});
+
+export const NewsSentimentFeedSchema = array(ArticleNewsSentimentSchema);
+
+export type ArticleNewsSentiment = Output<typeof ArticleNewsSentimentSchema>;
+
 export const ALPHA_VANTAGE_FUNCTIONS = {
   NEWS_SENTIMENT: 'NEWS_SENTIMENT'
 } as const;
